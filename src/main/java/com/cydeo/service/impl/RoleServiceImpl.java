2dd054783497e37a5a438d11186fe3b5cc41333a
Dilbar <dibux9@gmail.com>
2022-11-01 15:48:15 -0400

    ticketing-app

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,19 +2,23 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
@@ -25,9 +29,12 @@ public class RoleServiceImpl implements RoleService {
         // Go to repository and find a service(method) which gives me the roles from DB
         // How I will call any service here?
 
-        List<Role> roleList = roleRepository.findAll();
+        // I have Role entities from DB
+        // I need to convert those Role entities to DTOs
+        // I need to use ModelMapper
+        // I already create a class called RoleMapper and there are methods for me that will make this conversion
 
-        return roleList;
+        return roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
